e8e05645ea8c12b9c3ee7fcbc1763d4f5cdce6ec
aliazizi-code <devaliazizi@gmail.com>
2025-06-25 12:10:46 +0330

    Refactor urls.py to improve URL routing and add API schema endpoints

--- core/urls.py
+++ core/urls.py
@@ -1,22 +1,24 @@
-"""
-URL configuration for core project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.conf.urls.static import static
+from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    # document schema patterns
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    # document schema optional ui
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
+if settings.DEBUG and not settings.TESTING:
+    import debug_toolbar
+    from debug_toolbar.toolbar import debug_toolbar_urls
+    # urlpatterns += debug_toolbar_urls()
+    urlpatterns = [path('__debug__/', include(debug_toolbar.urls)),] + urlpatterns
