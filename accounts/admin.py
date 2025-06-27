52d59f76fd5e407bd430587730e2f94b18137313
aliazizi-code <devaliazizi@gmail.com>
2025-06-27 12:58:38 +0330

    Implement custom UserAdmin for managing user accounts in the admin interface

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .forms import UserCreationForm, UserChangeForm
+from . import models
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    list_display = ('phone', 'is_admin', 'first_name', 'last_name')
+    list_filter = ('is_admin',)
+    readonly_fields = ('last_login',)
+
+    fieldsets = (
+        ('Main', {'fields': ('phone', 'first_name', 'last_name', 'password')}),
+        ('Permissions', {'fields': ('is_active', 'is_admin', 'is_staff', 'is_superuser', 'last_login', 'groups', 'user_permissions')}),
+    )
+
+    add_fieldsets = (
+        (None, {'fields': ('phone', 'first_name', 'last_name', 'password1', 'password2')}),
+    )
+
+    search_fields = ('phone', 'first_name', 'last_name')
+    ordering = ('created_at',)
+    filter_horizontal = ('groups', 'user_permissions')
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        if not request.user.is_superuser:
+            form.base_fields['is_superuser'].disabled = True
+        return form
+
+
+admin.site.register(models.User, UserAdmin)
