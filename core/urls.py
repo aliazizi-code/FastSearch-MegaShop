ef58a4c687f5362dd67394dc7cf3d632e28a5624
aliazizi-code <devaliazizi@gmail.com>
2025-06-27 16:49:27 +0330

    Add missing URL pattern for accounts API in urlpatterns

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/accounts/', include('accounts.urls')),
     
     # document schema patterns
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
