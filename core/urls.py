8521efe24b91eeaf300934565f20850fba00527c
aliazizi-code <devaliazizi@gmail.com>
2025-07-02 17:49:06 +0330

    add products urls

--- core/urls.py
+++ core/urls.py
@@ -7,6 +7,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/accounts/', include('accounts.urls')),
+    path('api/products/', include('products.urls')),
     
     # document schema patterns
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
