ad8d5336af2e10ee0a0d1d1eb201082e652d5a5f
aliazizi-code <devaliazizi@gmail.com>
2025-07-02 14:57:50 +0330

    delete index,sv field for postgres

--- products/models.py
+++ products/models.py
@@ -1,5 +1,3 @@
-from django.contrib.postgres.indexes import GinIndex
-from django.contrib.postgres.search import SearchVectorField
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from taggit.managers import TaggableManager
@@ -37,12 +35,6 @@ class Product(models.Model):
         help_text=_('Cached tags as plain text for FTS')
     )
 
-    sv = SearchVectorField(
-        editable=False,
-        null=True,
-        help_text=_('Search vector field for full-text search')
-    )
-
     is_published = models.BooleanField(default=False, verbose_name=_('Is Published'))
     is_deleted = models.BooleanField(default=False, verbose_name=_('Is Deleted'))
 
@@ -51,39 +43,8 @@ class Product(models.Model):
     
     objects = ProductManager()
 
-    class Meta:
-        ordering = ['-created_at', 'id']
-        verbose_name = _('Product')
-        verbose_name_plural = _('Products')
-        unique_together = [('title', 'slug')]
-
-        indexes = [
-            GinIndex(fields=['sv'], name='product_sv_idx'),
-            models.Index(
-                fields=['is_published'],
-                name='published_products_idx',
-                condition=models.Q(is_published=True)
-            ),
-            models.Index(
-                fields=['is_deleted'],
-                name='deleted_products_idx',
-                condition=models.Q(is_deleted=False)
-            ),
-            models.Index(fields=['slug'], name='product_slug_btree_idx'),
-            models.Index(fields=['title'], name='product_title_idx'),
-        ]
-
     def __str__(self):
         return self.title
 
     def update_cached_tags(self):
-        self.cached_tags = ' '.join(self.tags.names())
-
-    def update_search_vector(self):
-        from django.contrib.postgres.search import SearchVector
-        Product.objects.filter(pk=self.pk).update(
-            sv=(
-                SearchVector('title', weight='A') +
-                SearchVector('cached_tags', weight='B')
-            )
-        )
+        self.cached_tags = ','.join(self.tags.names())
