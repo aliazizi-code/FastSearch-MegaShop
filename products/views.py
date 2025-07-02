c25142f8bd395b77caeaa1a040219e352fe10ea5
aliazizi-code <devaliazizi@gmail.com>
2025-07-02 21:22:30 +0330

    add search feature

--- products/views.py
+++ products/views.py
@@ -1,21 +1,23 @@
+from rest_framework import status, response
 from django_elasticsearch_dsl_drf.viewsets import DocumentViewSet
 from django_elasticsearch_dsl_drf.filter_backends import (
     FilteringFilterBackend,
-    SearchFilterBackend,
+    CompoundSearchFilterBackend,
     DefaultOrderingFilterBackend,
 )
 from django_elasticsearch_dsl_drf.pagination import PageNumberPagination
 
 from .documents import ProductDocument
-from .models import Product
+from .serializers import ProductDocumentSerializer
 
 
 class ProductDocumentView(DocumentViewSet):
     document = ProductDocument
+    serializer_class = ProductDocumentSerializer
     pagination_class = PageNumberPagination
     filter_backends = [
         FilteringFilterBackend,
-        SearchFilterBackend,
+        CompoundSearchFilterBackend,
         DefaultOrderingFilterBackend,
     ]
 
@@ -35,3 +37,11 @@ class ProductDocumentView(DocumentViewSet):
     }
 
     ordering = ('title',)
+    
+    def list(self, request, *args, **kwargs):
+        if not request.GET.get('search'):
+            return response.Response(
+                {"detail": 'Please provide a search query.'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        return super().list(request, *args, **kwargs)
