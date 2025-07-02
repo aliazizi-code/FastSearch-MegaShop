0592170bf86a7886dc5e00ab4feca6ec083fb1cd
aliazizi-code <devaliazizi@gmail.com>
2025-07-02 17:48:01 +0330

    Implement DocumentViewSet for Product with search, filter, and ordering support

--- products/views.py
+++ products/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django_elasticsearch_dsl_drf.viewsets import DocumentViewSet
+from django_elasticsearch_dsl_drf.filter_backends import (
+    FilteringFilterBackend,
+    SearchFilterBackend,
+    DefaultOrderingFilterBackend,
+)
+from django_elasticsearch_dsl_drf.pagination import PageNumberPagination
 
-# Create your views here.
+from .documents import ProductDocument
+from .models import Product
+
+
+class ProductDocumentView(DocumentViewSet):
+    document = ProductDocument
+    pagination_class = PageNumberPagination
+    filter_backends = [
+        FilteringFilterBackend,
+        SearchFilterBackend,
+        DefaultOrderingFilterBackend,
+    ]
+
+    search_fields = (
+        'title',
+        'description',
+        'cached_tags',
+    )
+
+    filter_fields = {
+        'is_published': 'is_published',
+        'is_deleted': 'is_deleted',
+    }
+
+    ordering_fields = {
+        'title': 'title.keyword',
+    }
+
+    ordering = ('title',)
