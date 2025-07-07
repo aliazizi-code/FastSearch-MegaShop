0e8bbc28849f253755dd90e46a135efc8b8f333f
aliazizi-code <devaliazizi@gmail.com>
2025-07-07 13:36:25 +0330

    refactor: enhance ProductDocument fields and analyzers for better indexing

--- products/documents.py
+++ products/documents.py
@@ -19,6 +19,11 @@ product_index.settings(
             "persian_stop": {
             "type": "stop",
             "stopwords":  "_persian_"
+            },
+            "edge_ngram_filter": {
+                "type": "edge_ngram",
+                "min_gram": 2,
+                "max_gram": 20
             }
         },
         "analyzer": {
@@ -33,6 +38,19 @@ product_index.settings(
                     "persian_stop",
                     "persian_stem"
                 ]
+            },
+            "rebuilt_persian_ngram": {
+                "tokenizer":   "standard",
+                "char_filter": ["zero_width_spaces"],
+                "filter": [
+                    "lowercase",
+                    "decimal_digit",
+                    "arabic_normalization",
+                    "persian_normalization",
+                    "persian_stop",
+                    "persian_stem",
+                    "edge_ngram_filter"
+                ]
             }
         }
     }
@@ -41,25 +59,42 @@ product_index.settings(
 
 @registry.register_document
 class ProductDocument(Document):
-    title = fields.TextField(
-        fields={
-            "fa": fields.TextField(analyzer='rebuilt_persian'),
-            "en": fields.TextField(),
-        }
+    title_fa = fields.TextField(
+    attr="title",
+    analyzer="rebuilt_persian",
+    search_analyzer="rebuilt_persian"
     )
-    description = fields.TextField(
-        fields={
-            "fa": fields.TextField(analyzer='rebuilt_persian'),
-            "en": fields.TextField(),
-        }
+    title_fa_ngram = fields.TextField(
+        attr="title",
+        analyzer="rebuilt_persian_ngram",
+        search_analyzer="rebuilt_persian"
     )
-    tags = fields.TextField(
-        attr='cached_tags',
-        fields={
-            "fa": fields.TextField(analyzer='rebuilt_persian'),
-            "en": fields.TextField(),
-        }
+    title_en = fields.TextField(attr="title")
+    
+    description_fa = fields.TextField(
+    attr="description",
+    analyzer="rebuilt_persian",
+    search_analyzer="rebuilt_persian"
     )
+    description_fa_ngram = fields.TextField(
+        attr="description",
+        analyzer="rebuilt_persian_ngram",
+        search_analyzer="rebuilt_persian"
+    )
+    description_en = fields.TextField(attr="description")
+    
+    tags_fa = fields.TextField(
+    attr="cached_tags",
+    analyzer="rebuilt_persian",
+    search_analyzer="rebuilt_persian"
+    )
+    tags_fa_ngram = fields.TextField(
+        attr="cached_tags",
+        analyzer="rebuilt_persian_ngram",
+        search_analyzer="rebuilt_persian"
+    )
+    tags_en = fields.TextField(attr="cached_tags")
+
     
     class Index:
         name = 'products'
@@ -67,7 +102,11 @@ class ProductDocument(Document):
         
     class Django:
         model = Product
-        fields = ['is_published', 'is_deleted']
+        fields = ['title', 'id']
         
     def get_queryset(self):
         return super().get_queryset().filter(is_published=True, is_deleted=False)
+    
+    def should_index_instance(self, instance):
+        return instance.is_published and not instance.is_deleted
+
