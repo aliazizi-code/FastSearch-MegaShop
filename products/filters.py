3d9dda27bdfebec2f1ab6408bcfffaa2a8627e56
aliazizi-code <devaliazizi@gmail.com>
2025-07-07 17:02:09 +0330

    feat: add FuzzySearchFilterBackend for enhanced search functionality

---
+++ products/filters.py
@@ -0,0 +1,26 @@
+from django_elasticsearch_dsl_drf.filter_backends import BaseSearchFilterBackend
+from elasticsearch_dsl.query import MultiMatch
+
+class FuzzySearchFilterBackend(BaseSearchFilterBackend):
+    def filter_queryset(self, request, queryset, view):
+        query = request.query_params.get('search', None)
+        if not query:
+            return queryset
+        
+        queryset =  queryset.query(
+            MultiMatch(
+                query=query,
+                fields=[
+                    'title_fa^3',
+                    'title_fa_ngram^2',
+                    'title_en',
+                    'description_fa',
+                    'tags_fa',
+                    'tags_fa_ngram',
+                ],
+                fuzziness='AUTO',
+                prefix_length=1,
+                type='best_fields',
+            )
+        )
+        return queryset
