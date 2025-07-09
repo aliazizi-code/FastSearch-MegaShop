668f4d44649308bccd0edcfe456e733325f07453
aliazizi-code <devaliazizi@gmail.com>
2025-07-09 16:22:55 +0330

    refactor: replace custom pagination with SearchAfterRelevancePagination for improved performance

--- products/views.py
+++ products/views.py
@@ -1,28 +1,16 @@
 from django_elasticsearch_dsl_drf.viewsets import DocumentViewSet
-from django_elasticsearch_dsl_drf.filter_backends import CompoundSearchFilterBackend
-from rest_framework.pagination import CursorPagination
 from rest_framework.exceptions import ValidationError
 
 from .documents import ProductDocument
 from .serializers import ProductDocumentSerializer
 from .filters import FuzzySearchFilterBackend
-
-
-class ProductListPagination(CursorPagination):
-    page_size = 16
-    page_size_query_param = 'page_size'
-    max_page_size = 48
-    
-    def get_ordering(self, request, queryset, view):
-        if queryset.query.order_by:
-            return queryset.query.order_by
-        return super().get_ordering(request, queryset, view)
+from .pagination import SearchAfterRelevancePagination
 
 
 class ProductDocumentView(DocumentViewSet):
     document = ProductDocument
     serializer_class = ProductDocumentSerializer
-    # pagination_class = ProductListPagination
+    pagination_class = SearchAfterRelevancePagination
     filter_backends = [
         FuzzySearchFilterBackend
     ]
